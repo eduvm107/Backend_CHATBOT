92ee0e4b0c33a8b7e3f0792e1a1d8a93d5597003
dessDario <22200222@ue.edu.pe>
2025-11-25 12:56:27 -0500

    Creacion de chatBot Ollama version 2

---
+++ ChatbotTCS.AdminAPI/Models/KnowledgeBase.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ChatbotTCS.AdminAPI.Models
+{
+    //  ignora cualquier otro dato extra inesperado
+    [BsonIgnoreExtraElements]
+    public class KnowledgeBase
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("content")]
+        public string Content { get; set; }
+
+        [BsonElement("category")]
+        public string Category { get; set; }
+
+        [BsonElement("embedding")]
+        public double[] Embedding { get; set; }
+
+       
+        // Aquí se guardará el puntaje de similitud que se traiga de MongoDB
+        [BsonElement("score")]
+        public double? Score { get; set; }
+    }
+}
